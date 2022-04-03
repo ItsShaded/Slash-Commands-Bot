964de61e94a4eaaff604a857d2087567120da18e
Shaded <72438879+ItsShaded@users.noreply.github.com>
2022-04-03 18:25:29 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,26 @@
+const {REST} = require("@discordjs/rest")
+const {Routes} = require("discord-api-types/v9")
+require("dotenv").config()
 module.exports = {
-    name: 'ready',
+    name: "ready",
     once: true,
-    async execute(client) {
-        console.log(`${client.user.username} est en ligne !`);
-    }
-}
+    execute(client, commands) {
+
+      // New Version
+      const CLIENT_ID = client.user.id;
+      const rest = new REST({
+        version: '9',
+      }).setToken(process.env.TOKEN);
+
+      (async ()  => {
+          try{
+              await rest.put(Routes.applicationCommands(CLIENT_ID), {
+                  body : commands
+              });
+              console.log(`Succesfully registered commands globally! ${client.guilds.cache.map(person => person.memberCount).reduce(function (s, v) { return s + (v || 0) }, 0)} users.`)
+          } catch (err) {
+            if (err)  console.error(err)
+          }
+      })()
+      }
+  }
