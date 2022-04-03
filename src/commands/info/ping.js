de74dde1f40fef783c73c062944a2f14cca42748
Shaded <72438879+ItsShaded@users.noreply.github.com>
2022-04-03 18:20:20 +0530

    Create ping.js

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,13 +1,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { MessageEmbed, client } = require('discord.js')
-
+const Discord = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription('Replies with pong !'),
+        .setName('ping')
+        .setDescription('Check the Bot API and Latency'),
     async execute(interaction) {
-        const embed = new MessageEmbed()
-        .setDescription(`pong`)
-        await interaction.reply({embeds: [embed]})
+        const embed = new Discord.MessageEmbed()
+            .setColor('#23272A')
+            .setTimestamp()
+            .setTitle(`Pong <:valid:844301736264204339>`)
+            .addField('Latancy:', `${(interaction.client.ws.ping).toFixed(0)} ms`, true)
+            .addField('API Latancy:', `${Date.now() - interaction.createdTimestamp}ms`)
+        interaction.reply({ embeds: [embed] , ephemeral: true })
     }
 }
